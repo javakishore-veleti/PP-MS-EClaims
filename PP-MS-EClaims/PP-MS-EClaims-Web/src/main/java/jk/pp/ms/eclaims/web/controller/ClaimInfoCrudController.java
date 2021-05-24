98018337ac59251bc3b3c5b22af4dd5cd8c51fb1
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-24 15:54:31 -0400

    Apache Kafka initial commit

--- PP-MS-EClaims/PP-MS-EClaims-Web/src/main/java/jk/pp/ms/eclaims/web/controller/ClaimInfoCrudController.java
+++ PP-MS-EClaims/PP-MS-EClaims-Web/src/main/java/jk/pp/ms/eclaims/web/controller/ClaimInfoCrudController.java
@@ -1,5 +1,9 @@
 package jk.pp.ms.eclaims.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,30 @@ public class ClaimInfoCrudController extends AppCrudController<ClaimInfo, ClaimI
 	@Autowired
 	public ClaimInfoCrudController(ClaimInfoCrudService service) {
 		super.setCrudService(service);
+	}
+
+	@Override
+	public List<ClaimInfoCrudDTO> populateInitialDataDomais() {
+
+		List<ClaimInfoCrudDTO> initialDataDomains = new ArrayList<ClaimInfoCrudDTO>();
+
+		ClaimInfoCrudDTO crudDTO = null;
+		ClaimInfo asset = null;
+
+		Random randomGenerator = new Random();
+
+		for (int ctr = 1; ctr <= 10; ++ctr) {
+			crudDTO = new ClaimInfoCrudDTO();
+
+			asset = new ClaimInfo();
+
+			asset.setMemberPK((long) randomGenerator.nextInt(10));
+
+			crudDTO.setDomain(asset);
+
+			initialDataDomains.add(crudDTO);
+		}
+
+		return initialDataDomains;
 	}
 }
